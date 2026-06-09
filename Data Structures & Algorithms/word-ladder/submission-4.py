37d1764491435fdac0616aac5fdbfc7c31e0096e
anupamsistla <83710425+anupamsistla@users.noreply.github.com>
2026-06-08 23:45:22 -0500

    Add: word-ladder - submission-4

---
+++ Data Structures & Algorithms/word-ladder/submission-4.py
@@ -0,0 +1,50 @@
+from collections import defaultdict, deque
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        edges = defaultdict(list)
+        visited = set()
+
+        for i in range(len(wordList)):
+            for j in range(len(wordList[i])):
+                key = ""
+                for k in range(len(wordList[i])):
+                    if j == k:
+                        key += "*"
+                    else:
+                        key+= wordList[i][k]
+                newKey = tuple(key)
+                edges[newKey].append(wordList[i])
+
+
+        queue = deque()
+        queue.append(beginWord)
+        visited.add(beginWord)
+
+        print(edges)
+
+        count = 1
+
+        while queue:
+            for i in range(len(queue)):
+                curr = queue.popleft()
+
+                if curr == endWord:
+                    return count
+
+                for i in range(len(curr)):
+                    key = ""
+                    for j in range(len(curr)):
+                        if i == j:
+                            key += "*"
+                        else:
+                            key += curr[j]
+                    
+                    currKey = tuple(key)
+                    
+                    for nei in edges[currKey]:
+                        if nei not in visited:
+                            queue.append(nei)
+                            visited.add(nei)
+            count += 1
+        return 0
+
