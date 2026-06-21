6084b09260688142841cc3686b262a10bc1617fa
anupamsistla <83710425+anupamsistla@users.noreply.github.com>
2026-06-21 16:46:24 -0500

    Add: foreign-dictionary - submission-0

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-0.py
@@ -0,0 +1,54 @@
+from collections import deque
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        adjList = {c: set() for w in words for c in w}
+
+        for i in range(len(words)-1):
+            w1 = words[i]
+            w2 = words[i+1]
+
+            minLen = min(len(w1), len(w2))
+
+            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
+                return ""
+            
+            for j in range(minLen):
+                if w1[j] != w2[j]:
+                    adjList[w1[j]].add(w2[j])
+                    break
+                
+        inDegree = {c: 0 for w in words for c in w}
+
+        for _, values in adjList.items():
+            for v in values:
+                inDegree[v] += 1
+
+        queue = deque()
+        visited = set()
+        
+        for k in inDegree.keys():
+            if inDegree[k] == 0:
+                queue.append(k)
+                visited.add(k)
+            
+        res = []
+
+        while queue:
+            curr = queue.popleft()
+            res.append(curr)
+
+            for v in adjList[curr]:
+                inDegree[v] -= 1
+
+                if inDegree[v] == 0:
+                    queue.append(v)
+                    visited.add(v)
+        
+        for c in inDegree.keys():
+            if c not in visited and inDegree[c] == 0:
+                res.append(c)
+                visited.add(c)
+        
+        print(res)
+        return "".join(res) if len(visited) == len(inDegree.keys()) else ""                    
+
