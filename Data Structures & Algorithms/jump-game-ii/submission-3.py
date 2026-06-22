404309e57642b2da66751a261eb3dbdefe6f081b
anupamsistla <83710425+anupamsistla@users.noreply.github.com>
2026-06-22 17:39:52 -0500

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.py
@@ -0,0 +1,18 @@
+from collections import deque
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        l,r = 0, 0
+        res = 0
+
+        while r < len(nums)-1:
+            farthest = 0
+
+            for j in range(l, r+1):
+                farthest = max(farthest, nums[j] + j)
+            
+            l = r + 1
+            r = farthest
+            res += 1
+        
+        return res
+            
