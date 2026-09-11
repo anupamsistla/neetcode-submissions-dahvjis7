1aa519816a204c86440f54966851b1ed8c2e923c
anupamsistla <83710425+anupamsistla@users.noreply.github.com>
2026-09-11 11:58:07 -0500

    Add: combinations - submission-2

---
+++ Data Structures & Algorithms/combinations/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def foo(self, i, n, k, curr, res):
+        if k == 0:
+            res.append(curr.copy())
+            return
+        
+        if i > n:
+            return
+
+        curr.append(i)
+        self.foo(i+1, n, k-1, curr, res)
+        curr.pop()
+        self.foo(i+1, n, k, curr, res)
+        return        
+    
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        self.foo(1, n, k, [], res)
+        return res
+    
