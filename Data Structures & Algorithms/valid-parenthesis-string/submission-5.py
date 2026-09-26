4b15262978653a0a5970a5bad287426e5290aea0
anupamsistla <83710425+anupamsistla@users.noreply.github.com>
2026-09-26 11:31:02 -0500

    Add: valid-parenthesis-string - submission-5

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-5.py
@@ -0,0 +1,31 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        leftParen = []
+        wildCard = []
+
+        for i in range(len(s)):
+            if s[i] == "(":
+                leftParen.append(i)
+            
+            elif s[i] == "*":
+                wildCard.append(i)
+            
+            else:
+                if leftParen:
+                    leftParen.pop()
+
+                else:
+                    if wildCard:
+                        wildCard.pop()
+                    
+                    else:
+                        return False
+
+        while leftParen and wildCard:
+            l, w = leftParen.pop(), wildCard.pop()
+
+            if w < l:
+                return False
+
+        return len(leftParen) == 0
+                
